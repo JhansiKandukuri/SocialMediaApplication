b066c08c0755972f49a57044a24cc763070ac18a
SwatiSinhaGH <73685347+SwatiSinhaGH@users.noreply.github.com>
2020-11-04 21:40:35 +0530

    Comments updated in exception class

--- SMA.Exception/Class1.cs
+++ SMA.Exception/Class1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+///<Summary>
+/// Custom Exception
+///</Summary>
 namespace SMA.Exception
 {
      public class SocialMediaException : ApplicationException
