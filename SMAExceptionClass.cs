e2b801116f10d06205e0d55e4d27d27c71a986d4
SwatiSinhaGH <73685347+SwatiSinhaGH@users.noreply.github.com>
2020-11-04 21:22:21 +0530

    Exception Class
    default and parametrized exception constructors to the base class Application excpetion

---
+++ SMAExceptionClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Custom Exception
+/// </summary>
+namespace SMA.Exception
+{
+    public class SocialMediaException : ApplicationException
+    {
+        public SocialMediaException() : base()
+        {
+
+        }
+        public SocialMediaException(string errorMsg) : base()
+        {
+
+        }
+    }
+}
