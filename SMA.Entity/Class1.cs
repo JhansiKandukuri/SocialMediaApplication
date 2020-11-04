207508c155eebb73606a2ef2ce10c305c186f0cc
SwatiSinhaGH <73685347+SwatiSinhaGH@users.noreply.github.com>
2020-11-04 21:57:07 +0530

    how to add explained
    comments added "entity layer"

--- SMA.Entity/Class1.cs
+++ SMA.Entity/Class1.cs
@@ -4,8 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+///<Summary>
+/// Entity Layer
+///</Summary>
 namespace SMA.Entity
 {
+    
     public class Class1
     {
     }
